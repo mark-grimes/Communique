aa01e60fea343cb8c3db885528ccfa2e85b24e13
Mark Grimes <mark.grimes@bristol.ac.uk>
2015-03-12 18:42:01 +0100

    Add more connection tests

--- unitTests/Client_test.cpp
+++ unitTests/Client_test.cpp
@@ -4,6 +4,8 @@
 #include <thread>
 #include <iostream>
 
+constexpr auto shortWait=std::chrono::milliseconds(50); // A waiting time used to allow connection etcetera
+
 SCENARIO( "Test that Client behaves as expected" )
 {
 	GIVEN( "A Client" )
@@ -13,12 +15,18 @@ SCENARIO( "Test that Client behaves as expected" )
 		WHEN( "I try to connect to ws://echo.websocket.org" )
 		{
 			REQUIRE_NOTHROW( myClient.connect( "ws://echo.websocket.org" ) );
-			std::this_thread::sleep_for( std::chrono::seconds(3) );
+			std::this_thread::sleep_for(shortWait);
 			REQUIRE_NOTHROW( myClient.disconnect() );
 		}
-//		WHEN( "I try to connect to a non-existent address" )
-//		{
-//			REQUIRE_THROWS( myClient.connect( "ws://blah.blahasdf.com" ) );
-//		}
+		WHEN( "I try to connect to the secured version wss://echo.websocket.org" )
+		{
+			REQUIRE_NOTHROW( myClient.connect( "wss://echo.websocket.org" ) );
+			std::this_thread::sleep_for(shortWait);
+			REQUIRE_NOTHROW( myClient.disconnect() );
+		}
+		WHEN( "I try to connect to a non-existent address" )
+		{
+			REQUIRE_THROWS( myClient.connect( "ws://blah.blahasdf.com" ) );
+		}
 	}
 }
