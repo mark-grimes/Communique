d1e02d04c620787a7b31453b46040588f59a9883
Mark Grimes <mark.grimes@bristol.ac.uk>
2015-07-26 20:45:11 +0100

    Rename unitTests directory to test

--- test/testinputs.cpp
+++ test/testinputs.cpp
@@ -2,5 +2,5 @@
 
 size_t testinputs::portNumber=9108;
 const std::chrono::milliseconds testinputs::shortWait=std::chrono::milliseconds(50);
-const std::string testinputs::testFileDirectory="unitTests/testData/";
+const std::string testinputs::testFileDirectory="test/testData/";
 
